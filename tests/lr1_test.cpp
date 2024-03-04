742beedabfe53e0fd0b0dd8f7653c1f7c1d237a6
YushkovaEvgenia <zhenyayushkova22@gmail.com>
2024-03-04 19:02:48 +0400

    change2

--- tests/lr1_test.cpp
+++ tests/lr1_test.cpp
@@ -36,7 +36,7 @@ TEST_F(CountAndSumTest, CTest1) {
     EXPECT_EQ(result.first, 5);
     EXPECT_EQ(result.second, 100);
 }
-
+/*
 // Пример теста111111
 TEST_F(CountAndSumTest, CTest11111) {
 // Вызываем функцию countAndSum с тестовыми данными
@@ -46,6 +46,7 @@ TEST_F(CountAndSumTest, CTest11111) {
     EXPECT_EQ(result.first, 5);
     EXPECT_EQ(result.second, 100);
 }
+*/
 /*
 // Пример теста 2
 TEST_F(CountAndSumTest, CTest2) {
